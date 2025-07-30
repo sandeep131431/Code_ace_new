d17d200dcb2174dc9764a252e461cc1c7141a836
Sandeep Kumar <skumar131431@gmail.com>
2025-07-30 19:20:10 +0530

    env

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL1 ||'http://localhost:3000', 
+    baseURL: process.env.VITE_API_URL1 ||'http://localhost:3000', 
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
--- frontend/src/utils/axiosClient2.js
+++ frontend/src/utils/axiosClient2.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL2 ||'http://localhost:3001', 
+    baseURL: process.env.VITE_API_URL2 ||'http://localhost:3001', 
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
